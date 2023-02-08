2bb018a949bfd31097860e85f6800bca66fb37b6
skerrey <sethkerrey@gmail.com>
2023-02-08 16:13:14 -0600

    Add routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import Signup from './Signup';
 import { Container } from 'react-bootstrap';
 import AuthProvider from '../contexts/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Login from './Login';
 
 function App() {
   return (
-    <AuthProvider>
       <Container className="d-flex align-items-center justify-content-center" style={{
         minHeight: "100vh"
       }}>
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   );
 }
 
