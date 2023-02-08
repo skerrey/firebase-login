e047221708c32080e55fafdb4ff173df898c18b1
skerrey <sethkerrey@gmail.com>
2023-02-08 17:48:03 -0600

    Create PrivateRoute.js

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navigate, Route } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function PrivateRoute({ children }) {
+  const { currentUser } = useAuth();
+  
+  return (
+    currentUser ? children : <Navigate to="/login" redirect />
+  )
+}
+
