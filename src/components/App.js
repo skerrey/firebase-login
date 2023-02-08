9a5d0249cbc9ab2ca5c28a02d1485538cde0a8d6
skerrey <sethkerrey@gmail.com>
2023-02-08 17:48:37 -0600

    Add forgot password, update profile, private route

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import AuthProvider from '../contexts/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Login from './Login';
+import PrivateRoute  from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
 
 function App() {
   return (
@@ -15,9 +18,11 @@ function App() {
           <Router>
             <AuthProvider>
               <Routes>
-                <Route path="/" element={<Dashboard />} />
+                <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
+                <Route path="/update-profile" element={<PrivateRoute><UpdateProfile /></PrivateRoute>}/>
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
               </Routes>
             </AuthProvider>
           </Router>
