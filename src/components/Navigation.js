280a03f0fa9b5f1520658b509729b29b2adff59c
skerrey <sethkerrey@gmail.com>
2023-02-09 19:22:13 -0600

    Create Navigation.js

---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+// Description: Navigation component
+
+import React, { useState} from 'react';
+import { 
+  Container, Nav, Navbar, NavDropdown
+} from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function Navigation() {
+  const { currentUser } = useAuth();
+  return (
+    <>
+      <Navbar fixed="top" bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">React-Bootstrap {currentUser.email}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home  </Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              {/* <NavDropdown title="title" id="basic-nav-dropdown"> */}
+              <NavDropdown title="" id="basic-nav-dropdown">
+              {/* <NavDropdown title={JSON.stringify(auth.userEmail).slice(1, -1)} id="basic-nav-dropdown"> */}
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
