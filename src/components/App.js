022f95f6c418c10fbddb95f980dda0f6809573db
skerrey <sethkerrey@gmail.com>
2023-02-10 10:26:37 -0600

    Updated app.js and imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,18 @@
 // Description: This is the main component of the application. It is the parent component of all other components. 
 
-import React from 'react';
-import Signup from './Signup';
-import { Container } from 'react-bootstrap';
-import AuthProvider from '../contexts/AuthContext';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProjectRoutes from "./ProjectRoutes";
+import Navigation from "./Navigation";
+import AuthProvider from "../contexts/AuthContext";
 
-// Import components
-import Dashboard from './Dashboard';
-import Login from './Login';
-import PrivateRoute  from './PrivateRoute';
-import ForgotPassword from './ForgotPassword';
-import UpdateProfile from './UpdateProfile';
 
 function App() {
   return (
     <>
-      <div className="container d-flex justify-content-center align-items-center" style={{
-        minHeight: "100vh"
-      }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Router>
-            <AuthProvider>
-              <Routes>
-                <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
-                <Route path="/update-profile" element={<PrivateRoute><UpdateProfile /></PrivateRoute>}/>
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-              </Routes>
-            </AuthProvider>
-          </Router>
-        </div>
-      </div>
+      <AuthProvider>
+        <Navigation />
+        <ProjectRoutes />
+      </AuthProvider>
+
     </>
   );
 }
