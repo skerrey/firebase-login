e7b3c3720b2f7e2ce79a5ab74240994ea582bd0d
skerrey <sethkerrey@gmail.com>
2023-02-10 10:26:17 -0600

    Added currentUser

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,18 +8,20 @@ import { useAuth } from '../contexts/AuthContext';
 
 export default function Navigation() {
   const { currentUser } = useAuth();
+
+
   return (
     <>
       <Navbar fixed="top" bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/">React-Bootstrap {currentUser.email}</Navbar.Brand>
+          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/">Home  </Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
               {/* <NavDropdown title="title" id="basic-nav-dropdown"> */}
-              <NavDropdown title="" id="basic-nav-dropdown">
+              <NavDropdown title={currentUser ? currentUser.email : "Login"} id="basic-nav-dropdown">
               {/* <NavDropdown title={JSON.stringify(auth.userEmail).slice(1, -1)} id="basic-nav-dropdown"> */}
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
