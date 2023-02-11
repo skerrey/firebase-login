767f83cd71960792374a8de41585803462a877d4
skerrey <sethkerrey@gmail.com>
2023-02-11 16:41:38 -0600

    Navbar testing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@
 
 import React, { useState} from 'react';
 import { 
-  Container, Nav, Navbar, NavDropdown
+  Container, Dropdown, Nav, Navbar, NavDropdown, Offcanvas
 } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
@@ -23,18 +23,19 @@ export default function Navigation() {
     }
   }
 
+  // Change expand to "xs, sm, md, lg, xl" for screen size
+  let expand = "sm";
 
   return (
     <>
-      <Navbar fixed="top" bg="light" expand="lg">
+      <Navbar bg="light" expand={expand}>
         <Container>
-          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">Firebase Login</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="basic-navbar-nav" alginRight>
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
 
-              {/* Conditional display if user logged in */}
               {!currentUser ? 
                 <Nav.Link href="/login">Login</Nav.Link> :               
                 <NavDropdown title={currentUser.email} id="basic-nav-dropdown">
@@ -50,6 +51,30 @@ export default function Navigation() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <Navbar  bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
     </>
   )
 }
