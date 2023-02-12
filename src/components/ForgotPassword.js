e20daf77363bcbc0a36850b30e99875db0753dd1
skerrey <sethkerrey@gmail.com>
2023-02-11 19:09:09 -0600

    Edit styling

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -28,27 +28,31 @@ export default function ForgotPassword() {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Password Reset</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          {message && <Alert variant="success">{message}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Rest Password
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Login</Link>
-          </div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
+      <div className="parent-container">
+        <div className="child-container">
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Password Reset</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              {message && <Alert variant="success">{message}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Button disabled={loading} className="w-100 mt-3" type="submit">
+                  Rest Password
+                </Button>
+              </Form>
+              <div className="w-100 text-center mt-3">
+                <Link to="/login">Login</Link>
+                <div className='mt-2'>
+                  Need an account? <Link to="/signup">Sign Up</Link>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
     </>
   )
