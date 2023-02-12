054c5ea44da797258144fe687bdc6a87774c3c46
skerrey <sethkerrey@gmail.com>
2023-02-11 19:09:15 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Container, Carousel } from 'react-bootstrap';
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className='d-flex justify-content-center align-items-center text-center' style={{
+        position: "relative",
+        fontSize: "5em",
+        color: "white",
+        height: "100vh",
+        zIndex: "2",
+      }}>
+        Welcome to Firebase Login
+      </div>
+    </div>
   )
 }
