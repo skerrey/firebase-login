ee9c3cc135df4b104278c6601757cda68b59162a
skerrey <sethkerrey@gmail.com>
2023-04-28 23:22:00 -0500

    Merge ProjectRoutes.js to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 // Description: This is the main component of the application. It is the parent component of all other components. 
 
-import ProjectRoutes from "./ProjectRoutes";
 import Navigation from "./Navigation";
 import Background from "./Background";
 import AuthProvider from "../contexts/AuthContext";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import components
+import Dashboard from './Dashboard';
+import Signup from './Signup';
+import Login from './Login';
+import PrivateRoute  from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
+import Home from './Home';
 
 function App() {
   return (
@@ -13,7 +21,14 @@ function App() {
       <AuthProvider>
         <Router>
           <Navigation />
-          <ProjectRoutes />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
+            <Route path="/settings" element={<PrivateRoute><UpdateProfile /></PrivateRoute>}/>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+          </Routes>
         </Router>
       </AuthProvider>
     </>
